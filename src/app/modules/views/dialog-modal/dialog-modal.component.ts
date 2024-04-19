a91849ae44ae7ceb6d9fd7a86fc82fbd8bcc878c
Keneth Cubas <kennethcubas93@gmail.com>
2024-04-19 17:02:46 -0600

    cositas en el estilo y dialog open

--- src/app/modules/views/dialog-modal/dialog-modal.component.ts
+++ src/app/modules/views/dialog-modal/dialog-modal.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-dialog-modal',
   templateUrl: './dialog-modal.component.html',
-  styleUrl: './dialog-modal.component.scss'
+  styleUrl: './dialog-modal.component.scss',
 })
 export class DialogModalView {
+  ref!: DynamicDialogRef;
+  constructor() {}
 
 }
--- src/app/modules/views/tabla-crud/tabla-crud.component.ts
+++ src/app/modules/views/tabla-crud/tabla-crud.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Cliente } from '../../interfaces/cliente.interface';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DialogModalView } from '../dialog-modal/dialog-modal.component';
 
 @Component({
   selector: 'app-tabla-crud',
@@ -7,18 +9,10 @@ import { Cliente } from '../../interfaces/cliente.interface';
   styleUrl: './tabla-crud.component.scss',
 })
 export class TablaCrudView {
+  ref!: DynamicDialogRef;
   clientes: Cliente[] = [];
 
-  /*
-      const clientes: Cliente[] = [
-      { codigo: 1, nombre: 'Juan', apellido: 'Pérez', saldo: 1000, activo: true },
-      { codigo: 2, nombre: 'María', apellido: 'Gómez', saldo: 500, activo: false },
-      { codigo: 3, nombre: 'Pedro', apellido: 'López', saldo: 2000, activo: true },
-      { codigo: 4, nombre: 'Ana', apellido: 'Martínez', saldo: 800, activo: false },
-      { codigo: 5, nombre: 'Carlos', apellido: 'Rodríguez', saldo: 1500, activo: true },
-    ];
-  */
-  constructor() {
+  constructor(private dialogService: DialogService) {
     this.clientes = [
       {
         codigo: 1,
@@ -59,10 +53,25 @@ export class TablaCrudView {
   }
 
   editCliente(clienteCode: number) {
-    console.log(`Editando cliente ${clienteCode}`);
+    this.ref = this.dialogService.open(DialogModalView, {
+      header: 'Editar cliente',
+      width: '50%',
+      closeOnEscape: true,
+      data: {
+        clienteCode,
+      },
+    });
   }
 
   deleteCliente(clienteCode: number) {
     console.log(`Borrando al cliente ${clienteCode}`);
+  }
+
+  newCliente() {
+    this.ref = this.dialogService.open(DialogModalView, {
+      header: 'Nuevo cliente',
+      width: '50%',
+      closeOnEscape: true,
+    });
   }
 }
